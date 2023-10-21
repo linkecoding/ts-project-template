e25f9d149f8a50d7e31e0497658e71727a514726
linkecoding <szher@qq.com>
2023-10-22 02:32:00 +0800

    feat: 修改项目名称和lint配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,12 @@
 const path = require('path');
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
   parserOptions: {
+    extraFileExtensions: ["cjs"],
     project: path.resolve(__dirname, './tsconfig.json'),
     tsconfigRootDir: __dirname,
     ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features
